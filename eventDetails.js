de10f3f1ac17d06a815f198b057573fb0df4948c
MR-DHRUV <dhruvgupta742@gmail.com>
2023-03-16 19:18:02 +0530

    Minor changes

--- eventDetails.js
+++ eventDetails.js
@@ -5,7 +5,7 @@ module.exports = Object.freeze({
     ambassadorName : ambassadorName, 
     eventName : eventName, 
     subject : "Event Certificate", // subject of email
-    body : `Hi,\nI hope this email finds you well. I am writing to follow up on the event "${eventName}" that we recently held. As a attendee in the event, you are entitled to receive a certificate acknowledging your participation.\nPlease find the certificate attached to this mail.\n\nThe certificate has been generated through the use of the program mrdhruv.xyz/gencert and has been sent automatically. We would greatly appreciate your support for this project by taking a moment to star the corresponding GitHub repository at mrdhruv.xyz/gencert/github. Thank you for your consideration!.\n\nThanks and Regards\n${ambassadorName}`, // body of email
+    body : `Hi,\nI hope this email finds you well. I am writing to follow up on the event "${eventName}" that we recently held. As a attendee in the event, you are entitled to receive a certificate acknowledging your participation.\nPlease find the certificate attached to this mail.\n\nThe certificate has been generated through the use of the program mrdhruv.xyz/gencert and has been sent automatically. I would greatly appreciate your support for this project by taking a moment to star the GitHub repository. Thank you for your consideration!.\n\nThanks and Regards\n${ambassadorName}`, // body of email
 });
 
 
